0143d8159aeb75c3ac922f220454cc1edcb483c7
Leonardo <leo1988g10@gmail.com>
2024-02-27 21:01:26 -0300

    MercadoLiebre

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+const port = 3003;
+
+app.use(express.static("public"));
+app.get("/", (req, res) => res.sendFile(path.resolve("./views/home.html")));
+app.get("/registro", (req, res) => res.sendFile(path.resolve("./views/registro.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve("./views/login.html")));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
